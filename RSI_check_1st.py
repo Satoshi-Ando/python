6766758d5f713c5243a63616311a39f976076212
Satoshi-Ando <satoshi.ando@ctc-g.co.jp>
2019-06-11 18:46:59 +0900

    RSI_check.py : complete

--- RSI_check_1st.py
+++ RSI_check_1st.py
@@ -28,32 +28,32 @@ no1 = len(l_001_i)
 for no1_c in range(len(l_001_i)):
 	no1_list = line_show_list.index(l_001_i[no1_c])
 	no1_list_e = no1_list + 1
-	print(k, file=codecs.open('C:/Users/z2085102/Desktop/result.txt', 'a', 'utf-8'))
-	print(*lines_strip[line_show_list[no1_list]:line_show_list[no1_list_e]], sep="\n", file=codecs.open('C:/Users/z2085102/Desktop/result.txt', 'a', 'utf-8'))
+	print(k)
+	print(*lines_strip[line_show_list[no1_list]:line_show_list[no1_list_e]], sep="\n")
 
 for no2_c in range(len(l_002_i)):
 	no2_list = line_show_list.index(l_002_i[no2_c])
 	no2_list_e = no2_list + 1
-	print(k, file=codecs.open('C:/Users/z2085102/Desktop/result.txt', 'a', 'utf-8'))
-	print(*lines_strip[line_show_list[no2_list]:line_show_list[no2_list_e]], sep="\n", file=codecs.open('C:/Users/z2085102/Desktop/result.txt', 'a', 'utf-8'))
+	print(k)
+	print(*lines_strip[line_show_list[no2_list]:line_show_list[no2_list_e]], sep="\n")
 
 for no3_c in range(len(l_003_i)):
 	no3_list = line_show_list.index(l_003_i[no3_c])
 	no3_list_e = no3_list + 1
-	print(k, file=codecs.open('C:/Users/z2085102/Desktop/result.txt', 'a', 'utf-8'))
-	print(*lines_strip[line_show_list[no3_list]:line_show_list[no3_list_e]], sep="\n", file=codecs.open('C:/Users/z2085102/Desktop/result.txt', 'a', 'utf-8'))
+	print(k)
+	print(*lines_strip[line_show_list[no3_list]:line_show_list[no3_list_e]], sep="\n")
 
 for no4_c in range(len(l_004_i)):
 	no4_list = line_show_list.index(l_004_i[no4_c])
 	no4_list_e = no4_list + 1
-	print(k, file=codecs.open('C:/Users/z2085102/Desktop/result.txt', 'a', 'utf-8'))
-	print(*lines_strip[line_show_list[no4_list]:line_show_list[no4_list_e]], sep="\n", file=codecs.open('C:/Users/z2085102/Desktop/result.txt', 'a', 'utf-8'))
+	print(k)
+	print(*lines_strip[line_show_list[no4_list]:line_show_list[no4_list_e]], sep="\n")
 
 for no5_c in range(len(l_005_i)):
 	no5_list = line_show_list.index(l_005_i[no5_c])
 	no5_list_e = no5_list + 1
-	print(k, file=codecs.open('C:/Users/z2085102/Desktop/result.txt', 'a', 'utf-8'))
-	print(*lines_strip[line_show_list[no5_list]:line_show_list[no5_list_e]], sep="\n", file=codecs.open('C:/Users/z2085102/Desktop/result.txt', 'a', 'utf-8'))
+	print(k)
+	print(*lines_strip[line_show_list[no5_list]:line_show_list[no5_list_e]], sep="\n")
 
 
 file.close()
